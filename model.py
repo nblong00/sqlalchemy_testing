97789305cf55d88eb534fdf3d75df7dee3bd6298
Blaise <blazzze0517@gmail.com>
2025-06-16 14:54:08 -0400

    Asks for user input, commits input to database, queries database to confirm recently added data is available.

--- model.py
+++ model.py
@@ -20,3 +20,11 @@ class User(Base):
 
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    user_name = input('Enter your name: ')
+    user_full_name = input('Enter your full_name: ')
+    user_nickname = input('Enter your nickname: ')
+    add_record = User(name=user_name, full_name=user_full_name, nickname=user_nickname)
+    print('\nUploading input data to database...')
+    session.add(add_record)
+    session.commit()
+    print(session.query(User).filter(User.name==user_name).one())
